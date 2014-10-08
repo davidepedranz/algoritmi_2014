547f4abfb8010ae3b7950df63c4b51608eb92ed5
Davide Pedranz <davide.pedranz@gmail.com>
2014-10-08 15:30:47 +0200

    score 80%

---
+++ lezione_1/sottomat.cpp
@@ -0,0 +1,183 @@
+//============================================================================
+// Name        : sottomat.cpp
+// Author      : Davide Pedranz
+// Version     : 2
+// Licence : GPL v2
+// Description : Lab_1, Task_3: sottomatrice massima
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int** alloc(int r, int c) {
+	int** matrix = new int*[r];
+
+	for (int i = 0; i < r; i++) {
+		matrix[i] = new int[c];
+	}
+
+	return matrix;
+}
+
+int** readMatrix(ifstream& in, int r, int c) {
+	int** matrix = new int*[r];
+
+	for (int i = 0; i < r; i++) {
+		matrix[i] = new int[c];
+
+		for (int j = 0; j < c; j++) {
+			in >> matrix[i][j];
+		}
+	}
+
+	return matrix;
+}
+
+void deleteMatrix(int** matrix, int r) {
+	for (int i = 0; i < r; i++) {
+		delete[] matrix[i];
+	}
+	delete[] matrix;
+}
+
+void printMatrix(int** matrix, int r, int c) {
+	cout << "matrix:" << endl;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int sum(int** matrix, int startRow, int startCol, int endRow, int endCol) {
+	int s = 0;
+
+	for (int i = startRow; i <= endRow; i++) {
+		for (int j = startCol; j <= endCol; j++) {
+			s += matrix[i][j];
+		}
+	}
+
+	return s;
+}
+
+int maxMatrix(int** matrix, int r, int c) {
+	int maxMatrix = 0;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			maxMatrix = max(maxMatrix, matrix[i][j]);
+		}
+	}
+	return maxMatrix;
+}
+
+int solve_1(int** matrix, int r, int c) {
+	int maxSum = 0;
+	int currentSum;
+
+	for (int i = 0; i < r; i++) {
+		for (int j = i; j < r; j++) {
+			for (int k = 0; k < c; k++) {
+				for (int l = k; l < c; l++) {
+					currentSum = sum(matrix, i, k, j, l);
+					maxSum = max(currentSum, maxSum);
+				}
+			}
+		}
+	}
+
+	return maxSum;
+}
+
+int solve_2(int** matrix, int r, int c) {
+	int** sumMatrix = alloc(r, c);
+
+	// fill sumMatrix
+	sumMatrix[0][0] = matrix[0][0];
+	for (int i = 1; i < r; i++) {
+		sumMatrix[i][0] = matrix[i][0] + sumMatrix[i - 1][0];
+	}
+	for (int j = 1; j < c; j++) {
+		sumMatrix[0][j] = matrix[0][j] + sumMatrix[0][j - 1];
+	}
+	for (int i = 1; i < r; i++) {
+		for (int j = 1; j < c; j++) {
+			sumMatrix[i][j] = matrix[i][j] + sumMatrix[i][j - 1]
+			+ sumMatrix[i - 1][j] - sumMatrix[i - 1][j - 1];
+		}
+	}
+
+	// debug
+	// printMatrix(sumMatrix, r, c);
+
+	int maxSum = 0;
+	int currentSum;
+
+	// TODO: duplicare ciclo e fare partire
+	// il primo da 0 fino a 1
+	// il secondo da i a n
+
+	for (int i = 0; i < r; i++) {
+		for (int k = i; k < r; k++) {
+			for (int j = 0; j < c; j++) {
+
+				bool goOn = true;
+				for (int l = j; l < c && goOn; l++) {
+					currentSum = sumMatrix[k][l]
+					- (i == 0 ? 0 : sumMatrix[i - 1][l])
+					- (j == 0 ? 0 : sumMatrix[k][j - 1])
+					+ (i != 0 && j != 0 ? sumMatrix[i - 1][j - 1] : 0);
+
+					if(currentSum < 0) {
+						goOn = false;
+					} else {
+						maxSum = max(currentSum, maxSum);
+					}
+				}
+			}
+		}
+	}
+
+	deleteMatrix(sumMatrix, r);
+
+	return maxSum;
+}
+
+int main() {
+
+	// stream input, output
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+
+	// matrix dimensions
+	int r, c;
+
+	// read matrix dimension
+	in >> r >> c;
+
+	// read matrix
+	int** matrix = readMatrix(in, r, c);
+
+	// debug
+	// printMatrix(matrix, r, c);
+
+	int maxSum;
+
+	// v1
+	// maxSum = solve_1(matrix, r, c);
+
+	// v2
+	maxSum = solve_2(matrix, r, c);
+
+	// cout << "Somma max: " << maxSum << endl;
+
+	// return output
+	out << maxSum << endl;
+
+	// dealloca
+	deleteMatrix(matrix, r);
+
+	return 0;
+}
