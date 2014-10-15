fb2057313ed5b2268252fe46b59a5fbb053e4b8a
Davide Pedranz <davide.pedranz@gmail.com>
2014-10-15 20:56:38 +0200

    Add sortpesato.cpp.

--- lezione_2/sortpesato.cpp
+++ lezione_2/sortpesato.cpp
@@ -1,29 +1,40 @@
 //============================================================================
 // Name        : sortpesato.cpp
 // Author      : Davide Pedranz
-// Version     : 1
+// Version     : 1n
 // Licence     : GPL v2
-// Description : Lab_2, Task_3: sortpesato
+// Description : See statement.
 //============================================================================
 
-#include <iostream>
+// #include <iostream>
 #include <fstream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-void printArray(int* array, int n) {
-	cout << "Array: ";
-	for(int i = 0; i < n; i++) {
-		cout << array[i] << " ";
-	}
-	cout << endl;
-}
+// struct Result {
+// 	int swaps;
+// 	int cost;
 
-void swap(int& a, int& b) {
-	int tmp = a;
-	a = b;
-	b = tmp;
-}
+// 	Result() {
+// 		swaps = 0;
+// 		cost = 0;
+// 	}
+// };
+
+struct Block {
+	int value;
+	bool visited;
+
+	Block(int v) {
+		visited = false;
+		value = v;
+	}
+
+	int getValue() {
+		visited = true;
+		return value;
+	}
+};
 
 // returns min element index
 int posMin(int* a, int start, int end) {
@@ -40,14 +51,15 @@ int posMin(int* a, int start, int end) {
 	return pos;
 }
 
-// numero di scambi fatti da algoritmo selectionSort
-int computeTurns(int* a, int* o, int n) {
+
+// numero di scambi fatti da algoritmo selectionSort (mettendo minimo in cima)
+int computeSwaps(int* a, int n) {
 	int swaps = 0;
 
 	int indexMin;
 	for(int i = 0; i < n; i++) {
 		indexMin = posMin(a, i, n);
-			if(indexMin != i) {
+		if(indexMin != i) {
 			swap(a[i], a[indexMin]);
 			swaps++;
 		}
@@ -55,11 +67,109 @@ int computeTurns(int* a, int* o, int n) {
 	return swaps;
 }
 
-// TODO: implement
-int computeCost(int* a, int* o, int n) {
+// ======================================================================================================================
+// ======================================================================================================================
 
-	return 0;
+// void printArray(int* array, int n) {
+// 	for(int i = 0; i < n; i++) {
+// 		cout << array[i] << " ";
+// 	}
+// 	cout << endl;
+// }
+
+// void printVV(vector<vector<int>>& vv) {
+// 	int i = 1;
+// 	for(vector<int> v : vv) {
+// 		cout << "vettore " << i++ << ":\t";
+// 		for(int el : v) {
+// 			cout << el << " ";
+// 		}
+// 		cout << endl;
+// 	}
+// }
+
+// void printV(vector<int>& v) {
+// 	for(int el : v) {
+// 		cout << el << " ";
+// 	}
+// 	cout << endl;
+// }
+
+// wrapper
+int computeCost(int* a, int n) {
+	Block** b = new Block*[n+1];
+
+	vector<vector<int>> vv;
+
+	for(int i = 1; i <= n; i++) {
+		b[i] = new Block(a[i-1]);
+	}
+
+	// printArray(a, n);
+	// for(int i = 1; i <= n; i++) {
+	// 	cout << b[i] -> value << " ";
+	// }
+	// cout << endl;
+
+	for(int i = 1; i <= n; i++) {
+		if(!b[i]->visited && b[i]->value != i) {
+			vector<int> v;
+
+			Block* current = b[i];
+			int value = current->getValue();
+			v.push_back(value);
+
+			Block* next = b[value];
+
+			while(!next->visited) {
+				int val = next->getValue();
+				v.push_back(val);
+				next = b[val];
+			}
+
+			vv.push_back(v);
+		}
+	}
+
+	// printVV(vv);
+
+	int totalCost = 0;
+
+	for(auto v : vv) {
+		// cout << endl;
+		// printV(v);
+
+		int sum = 0;
+		int min = n+10;
+		for(int el : v) {
+			sum += el;
+			if(el < min) {
+				min = el;
+			}
+		}
+
+		int costA = sum - min + min * (v.size() - 1);
+		// cout << "cost A: " << costA << endl;
+
+		int costB = sum - min + 1 * (v.size() - 1) + 2 * (1 + min);
+		// cout << "cost B: " << costB << endl;
+
+		int realCost = costA < costB ? costA : costB;
+		// cout << "realCost: " << realCost << endl;
+
+		totalCost += realCost;
+	}
+
+	for(int i = 0; i < n; i++) {
+		delete b[i];
+	}
+	delete[] b;
+
+	return totalCost;
 }
+
+// ======================================================================================================================
+// ======================================================================================================================
 
 int main() {
 
@@ -70,24 +180,23 @@ int main() {
 	int n;
 	in >> n;
 
-	int* array = new int[n];
-	int* ordered = new int[n];
+	int* a = new int[n];
+	int* b = new int[n];
 
+	// read array from input file
 	for(int i = 0; i < n; i++) {
-		in >> array[i];
-		ordered[i] = array[i];
+		in >> a[i];
+		b[i] = a[i];
 	}
 
-	sort(ordered, ordered + n);
+	int swaps = computeSwaps(a, n);
+	int cost = computeCost(b, n);
 
-	int turns = computeTurns(array, ordered, n);
-	int price = computeCost(array, ordered, n);
+	// return
+	out << swaps << " " << cost << endl;
 
-	// write output
-	out << turns << " " << price << endl;
-
-	// clean memory
-	delete[] array;
+	delete[] a;
+	delete[] b;
 
 	return 0;
 }
