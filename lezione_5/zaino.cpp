fe4bdaeaadc4acbf51ee8e7b17f88904f349c5b2
Davide Pedranz <davide.pedranz@gmail.com>
2014-11-30 10:11:02 +0100

    Add zaino.cpp - zaino con programmazione dinamica

--- lezione_5/zaino.cpp
+++ lezione_5/zaino.cpp
@@ -8,43 +8,50 @@
 
 #include <fstream>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
-
-const int VOID = -3;
-const int INF = 99999999;
-
-int zaino(int** s, int* pesi, int* valori, int i, int c) {
-
-	if (c < 0) {
-		return -INF;
-	}
-
-	if (c == 0 || i < 0) {
-		return 0;
-	}
-
-	if (s[i][c] == VOID) {
-		int prendo = zaino(s, pesi, valori, i-1, c - pesi[i]) + valori[i];
-		int no = zaino(s, pesi, valori, i-1, c);
-		s[i][c] = max(prendo, no);
-	}
-
-	return s[i][c];
-}
 
 int zaino(int* pesi, int* valori, int n, int c) {
 
-	int** s = new int*[n];
-	for(int i = 0; i < n; i++) {
-		s[i] = new int[c+1];
-		fill(s[i], s[i] + c + 1, VOID);
+	// tabella per la programmazione dinamica
+	// ridotta a 2 righe per risparmiare memoria e tempo
+	int** s = new int*[2];
+	s[0] = new int[c+1];
+	s[1] = new int[c+1];
+
+	fill(s[0], s[0]+c+1, 0);
+	fill(s[1], s[1]+c+1, 0);
+
+	int i;
+
+	for(i = 0; i < n; i++) {
+		int ii = abs(i-1) % 2;
+
+		// riempio tutta la riga
+		// NB: alcune caselle possono NON avere senso
+		// in questo caso ci sarà scritto 0
+		for(int j = 0; j < c+1; j++) {
+
+			int non_lo_prendo = s[ii][j];
+			int lo_prendo = (j + pesi[i] <= c) ? s[ii][j + pesi[i]] + valori[i] : -99999999;
+
+			s[i % 2][j] = max(lo_prendo, non_lo_prendo);
+		}
 	}
 
-	int result = zaino(s, pesi, valori, n-1, c);
+	i = abs(i-1) % 2;
 
-	for(int i = 0; i < n; i++) {
-		delete[] s[i];
+	// il risultato è il massimo valore che posso fare
+	// considerando tutti gli oggetti, quindi si trova
+	// sull'ultima riga
+	int result = s[i][0];
+	for(int j = 1; j < c+1; j++) {
+		result = max(s[i][j], result);
 	}
+
+	
+	delete[] s[0];
+	delete[] s[1];
 	delete[] s;
 
 	return result;
